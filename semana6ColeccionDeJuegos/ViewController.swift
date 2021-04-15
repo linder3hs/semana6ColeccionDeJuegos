d2c44451f3f9029d8968bc8f42f63e5d5ec13e26
linder3hs <linderhassinger00@gmail.com>
2021-04-15 09:54:58 -0500

    Initial Commit

---
+++ semana6ColeccionDeJuegos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  semana6ColeccionDeJuegos
+//
+//  Created by Linder Hassinger on 15/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
